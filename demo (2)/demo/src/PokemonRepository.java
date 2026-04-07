ac22b6bf01733b9e0407807e4c48783938e96cb6
Jennifer Rebeca Alves de Barros <jenniferrebecaab@gmail.com>
2026-04-06 22:29:43 -0300

    Add PokemonRepository interface for JPA access

---
+++ demo (2)/demo/src/PokemonRepository.java
@@ -0,0 +1,7 @@
+import com.example.demo.model.Pokemon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
+}
