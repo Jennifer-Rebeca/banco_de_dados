49159fc9e7791e3c811ff2478484d14a0697b94e
Jennifer Rebeca Alves de Barros <jenniferrebecaab@gmail.com>
2026-04-06 22:53:10 -0300

    Delete demo (2)/demo/src/PokemonInstanceRepository.java

--- demo (2)/demo/src/PokemonInstanceRepository.java
+++
@@ -1,7 +0,0 @@
-import com.example.demo.model.PokemonInstance;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PokemonInstanceRepository extends JpaRepository<PokemonInstance, Long> {
-}
