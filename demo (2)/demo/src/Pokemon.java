a07f6bffe4b8c34bf1e7df4ed5e34721c131438b
Jennifer Rebeca Alves de Barros <jenniferrebecaab@gmail.com>
2026-04-06 22:27:53 -0300

    Add Pokemon class with JPA annotations

---
+++ demo (2)/demo/src/Pokemon.java
@@ -0,0 +1,36 @@
+import jakarta.persistence.*;
+
+public class Pokemon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer numero;
+    private String nome;
+
+    public Pokemon() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
